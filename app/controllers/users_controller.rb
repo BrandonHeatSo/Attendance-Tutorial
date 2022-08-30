955e7adaf41b79f8c951c2bed4e5060ea881c411
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2022-08-30 11:53:05 +0000

    usersコントローラーにupdateアクションを追記して ifにはupdate_attribute（引数はuser_params）で更新するユーザー情報がＯＫか検索 else（ダメだったら）editにrenderするよう記述 することで更新が失敗した場合は編集ページを再描画するよう実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,24 +11,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user # 保存成功後、ログインします。
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
       render :new
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      #
+      # 更新に成功した場合の処理を記述します。
     else
-      render :edit
+      render :edit      
     end
   end
 
