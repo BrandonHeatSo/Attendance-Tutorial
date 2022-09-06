4e607639a1c3795144c9e5d25e28072a53f482fa
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2022-09-06 14:40:54 +0000

    出勤登録ボタンの作成その４ （打刻＝出勤ボタン押した後の出勤時刻の表示整理（秒数までは不要）＆出勤登録成功後のユーザー情報showへのリダイレクト時には出勤時刻が表示されるようビュー処理）
    １：「controllers/attendances_controller.rb」内の２つ目のif文内の条件部分「started_at: Time.current」直後に「change（sec: 0）」を追記して秒数表示カット
    ２：「views/users/show.html.erb」内のtbodyブロック内のtd４行目にERB文「l（day.started_at, format: :time） if day.started_at.present？」を記述
    （後置きifによってstarted_at出勤時刻の値が存在する場合のみ表示できるよう制御）

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -5,7 +5,7 @@ class AttendancesController < ApplicationController
     @attendance = Attendance.find(params[:id])
     # 出勤時間が未登録であることを判定します。
     if @attendance.started_at.nil?
-      if @attendance.update_attributes(started_at: Time.current)
+      if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
         flash[:info] = "おはようございます！"
       else
         flash[:danger] = "勤怠登録に失敗しました。やり直してください。"
